e15e546897f7d3f65a1a94ec7ce4c0e8d323bf76
Freddy-Mounir <jrfreddie@live.com>
2016-10-25 16:58:21 +0200

    Singleton

---
+++ DesignPatterns/DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _singletonObject;
+        protected Singleton()
+        {
+        }
+
+        public static Singleton CreateSingleton()
+        {
+            if (_singletonObject == null)
+            {
+                _singletonObject = new Singleton();
+            }
+           return _singletonObject;
+        }
+    }
+}
---
+++ DesignPatterns/SingletonProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPatterns.Singleton;
+
+namespace SingletonProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton firstObject = Singleton.CreateSingleton();
+            Singleton secondObject = Singleton.CreateSingleton();
+
+            // both objects should equal each other
+            if (firstObject == secondObject)
+            {
+                Console.WriteLine("first object equal second object , singleton succeeded");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed");
+            }
+        }
+    }
+}
